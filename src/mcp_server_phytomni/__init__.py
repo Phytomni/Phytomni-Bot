692ec04eb8aaaf6b8be7069a7cc27678bda299f5
sssxie <xieshang0608@gmail.com>
2025-07-12 14:53:27 +0800

    ✨ Feat: add task_manager functions

--- src/mcp_server_phytomni/__init__.py
+++ src/mcp_server_phytomni/__init__.py
@@ -9,11 +9,13 @@ from .deep_genome_agents import async_gene_function
 from .in_silico_research_agents import in_silico_research
 from .knowledge_agents import multi_retrieve, multi_retrieve_generate
 from .review_agents import deep_research
+from .task_manager import create_task, TaskManager, update_task
 from .utils import get_prompt, get_token, split_list
 from .utils import create_output_dir, get_data_list
 
-__all__ = ['async_gene_function', 'phyto_chat', 'config', 'deep_research',
-           'get_prompt', 'get_token', 'in_silico_research', 'multi_retrieve',
-           'multi_retrieve_generate', 'nl2sql', 'retrieve_plan_submit',
-           'rewrite_nl2sql', 'split_list',
+__all__ = ['async_gene_function', 'phyto_chat', 'config', 'create_task',
+           'deep_research', 'get_prompt', 'get_token', 'in_silico_research',
+           'multi_retrieve', 'multi_retrieve_generate', 'nl2sql',
+           'retrieve_plan_submit', 'rewrite_nl2sql', 'split_list',
+           'TaskManager', 'update_task',
            'create_output_dir', 'get_data_list']
