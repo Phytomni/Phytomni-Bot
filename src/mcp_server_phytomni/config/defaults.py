0b7abbeb7095b1affe7e04c0e4e9697592e8979a
sssxie <xieshang0608@gmail.com>
2025-07-14 03:20:45 +0800

    ♻️ Refactor: refactor submit function

--- src/mcp_server_phytomni/config/defaults.py
+++ src/mcp_server_phytomni/config/defaults.py
@@ -194,17 +194,17 @@ class AnalystConfig(KnowledgeConfig):
     """
     OBS_SERVER: str = Field('https://obs.cn-east-3.myhuaweicloud.com')
     BUCKET_NAME: str = Field('phytomni')
-    OUTPUT_DIR: str = Field('/obs/phytomni/agent_data/test/test/')
+    OUTPUT_DIR: str = Field('/obs/phytomni/agent_data/test/')
     COMPUTE_RESOURCE: Literal['small', 'medium', 'large'] = Field('small')
     TASK_NAME: str = Field('analyst-agents-task')
-    APP_ID: Dict[str, str] = Field({
-        'small': 'fa83143f-5e07-11f0-bbb4-fa163e7f72d1',
-        'medium': '1d1b3dc5-5e08-11f0-bbb4-fa163e7f72d1',
-        'large': '31b31aac-5e08-11f0-bbb4-fa163e7f72d1'})
     RESOURCE: Dict[str, Dict[str, int]] = Field({
         'small': {'cpu': 4, 'memory': 16},
         'medium': {'cpu': 8, 'memory': 32},
         'large': {'cpu': 16, 'memory': 64}})
+    APP_ID: Dict[str, str] = Field({
+        'small': 'fa83143f-5e07-11f0-bbb4-fa163e7f72d1',
+        'medium': '1d1b3dc5-5e08-11f0-bbb4-fa163e7f72d1',
+        'large': '31b31aac-5e08-11f0-bbb4-fa163e7f72d1'})
     EXECUTE_CODE: bool = Field(True)
     POLL_INTERVAL: float = Field(300)
     MAX_POLL: float = Field(86400)
