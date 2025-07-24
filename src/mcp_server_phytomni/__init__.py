330eac8a0680a3516a956d2f793a031af3408cc4
sssxie <xieshang0608@gmail.com>
2025-07-25 02:39:03 +0800

    ♻️ Refactor: rename async_gene_function to gene_function

--- src/mcp_server_phytomni/__init__.py
+++ src/mcp_server_phytomni/__init__.py
@@ -6,7 +6,7 @@ from .analyst_agents import create_output_dir, get_data_list
 from .analyst_agents import retrieve_plan_submit, wait_for_completion
 from .chat_agents import phyto_chat
 from .data_agents import nl2sql, rewrite_nl2sql
-from .deep_genome_agents import async_gene_function
+from .deep_genome_agents import gene_function
 from .in_silico_research_agents import in_silico_research
 from .knowledge_agents import multi_retrieve, multi_retrieve_generate
 from .knowledge_agents import response_to_string
@@ -14,9 +14,9 @@ from .review_agents import deep_research
 from .task_manager import create_task, TaskManager, update_task
 from .utils import get_prompt, get_token, split_list
 
-__all__ = ['async_gene_function', 'config', 'create_output_dir', 'create_task',
-           'deep_research', 'get_data_list', 'get_prompt', 'get_token',
+__all__ = ['config', 'create_output_dir', 'create_task', 'deep_research',
+           'gene_function', 'get_data_list', 'get_prompt', 'get_token',
            'in_silico_research', 'multi_retrieve', 'multi_retrieve_generate',
-           'nl2sql', 'phyto_chat', 'response_to_string', 'retrieve_plan_submit',
-           'rewrite_nl2sql', 'split_list', 'TaskManager', 'update_task',
-           'wait_for_completion']
+           'nl2sql', 'phyto_chat', 'response_to_string',
+           'retrieve_plan_submit', 'rewrite_nl2sql', 'split_list',
+           'TaskManager', 'update_task', 'wait_for_completion']
