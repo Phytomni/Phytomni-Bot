99e55121d304a7b0b2fc6e368a0e63f8de757f20
Boundless1 <1183907619@qq.com>
2025-08-06 16:26:41 +0800

    feat: add new task----enrichment task

--- src/mcp_server_phytomni/deep_genome_agents.py
+++ src/mcp_server_phytomni/deep_genome_agents.py
@@ -21,7 +21,7 @@ Key functionalities include:
 """
 import asyncio
 from collections import deque
-from json import dumps, loads
+from json import dumps, loads, dump
 from pathlib import Path
 from random import randint
 from threading import Thread
@@ -30,7 +30,7 @@ from uuid import uuid1
 
 from mcp.shared.exceptions import McpError
 
-from .analyst_agents import create_output_dir, download_obs_out, get_data_list
+from .analyst_agents import create_output_dir, upload_analyst_agents_data, download_obs_out, get_data_list
 from .analyst_agents import submit, wait_for_completion
 from .chat_agents import phyto_chat
 from .config.defaults import DeepGenomeConfig
@@ -2322,6 +2322,127 @@ async def haplotypes_analysis(
         max_poll=max_poll,
     )
     return {'haplotypes_task': haplotypes_task}
+
+
+async def enrichment_analysis(
+    species: str,
+    gene_list: List[str],
+    user_id: str = dgc.USER_ID,
+    batch: bool = dgc.BATCH,
+    prompt_file: str = dgc.PROMPT_FILE,
+    deepgenome_data: str = dgc.DEEPGENOME_DATA,
+    output_dir: str = dgc.OUTPUT_DIR,
+    model_url: str = sc.CODER_URL,
+    model_name: str = sc.CODER_MODEL,
+    coder_api_key: str = sc.CODER_API_KEY.get_secret_value(),
+    access_key_id: str = sc.AccessKeyID.get_secret_value(),
+    secret_access_key: str = sc.SecretAccessKey.get_secret_value(),
+    obs_server: str = dgc.OBS_SERVER,
+    bucket_name: str = dgc.BUCKET_NAME,
+    analysis_url: str = dgc.ANALYSIS_URL,
+    region: str = dgc.ANALYSIS_REGION,
+    resource_dict: Dict[str, Dict[str, int]] = dgc.RESOURCE,
+    app_id_dict: Dict[str, str] = dgc.APP_ID,
+    timeout: float = dgc.TIMEOUT,
+    retriable_codes: List[int] = dgc.RETRIABLE_CODES,
+    max_retries: int = dgc.MAX_RETRIES,
+    max_poll: float = dgc.MAX_POLL,
+) -> dict:
+    """Perform a enrichment analysis of a gene.
+
+    This function submits a task to perform a enrichment analysis of the
+    given gene. It constructs a goal description and data list, and then
+    calls the `submit` function to initiate the analysis.
+
+    Args:
+        species: The species of the gene.
+        gene_list: The identifier of the gene list to analyze.
+        user_id: The user identifier for this task.
+        batch: Flag indicating whether the operation is part of a batch.
+        prompt_file: The path to the prompt file.
+        deepgenome_data: The path to the deep genome data.
+        output_dir: The directory to store output files.
+        model_url: The URL for the model service.
+        model_name: The name of the model.
+        coder_api_key: The API key for the coder service.
+        access_key_id: The access key identifier for OBS.
+        secret_access_key: The secret access key for OBS.
+        obs_server: The OBS server URL.
+        bucket_name: The OBS bucket name.
+        analysis_url: The URL for the analysis service.
+        region: The region for the analysis service.
+        resource_dict: A dictionary of resource configurations.
+        app_id_dict: A dictionary of application identifiers.
+        timeout: Request timeout in seconds.
+        retriable_codes: List of HTTP status codes that trigger a retry.
+        max_retries: Maximum number of retry attempts.
+        max_poll: Maximum duration in seconds to monitor the task.
+
+    Returns:
+        A dictionary containing the task information for the haplotype
+        analysis.
+    """
+    goal_description = get_prompt(prompt_file, 'user/enrichment_analysis')
+    data_list = get_data_list(deepgenome_data, 'enrichment_analysis', species)
+    if not batch:
+        if not user_id:
+            user_id = uuid1()
+        output_dir = create_output_dir(
+            user_id=user_id,
+            task='enrichment_task',
+            access_key_id=access_key_id,
+            secret_access_key=secret_access_key,
+            obs_server=obs_server,
+            bucket_name=bucket_name,
+        )
+    gene_list_file = Path(f'enrichment_gene_list_{uuid1()}.txt')
+    try:
+        gene_list = ['gene_id'] + gene_list
+        with open(gene_list_file, 'w', encoding='utf-8') as gene_list_f:
+            gene_list_f.write('\n'.join(gene_list))
+        data_path = upload_analyst_agents_data(
+            analyst_agents_datapath=str(gene_list_file),
+            access_key_id=access_key_id,
+            secret_access_key=secret_access_key,
+            obs_server=obs_server,
+            bucket_name=bucket_name,
+            )
+    except OSError as exc:
+        raise OSError('Data information upload obs error.') from exc
+    finally:
+        if gene_list_file.exists():
+            gene_list_file.unlink()
+    data_path = data_path.split(':/')[-1]
+    key_path = f'/obs/phytomni/{data_path}'
+    data_list[key_path] = 'List of user-supplied gene enrichment genes'
+    meta = get_prompt(prompt_file, 'user/enrichment_analysis_meta')
+    enrichment_task = await submit(
+        goal_description=goal_description,
+        data_list=data_list,
+        user_id=user_id,
+        is_create_dir=False,
+        output_dir=output_dir,
+        meta=meta,
+        execute_code=True,
+        model_url=model_url,
+        model_name=model_name,
+        coder_api_key=coder_api_key,
+        access_key_id=access_key_id,
+        secret_access_key=secret_access_key,
+        obs_server=obs_server,
+        bucket_name=bucket_name,
+        analysis_url=analysis_url,
+        region=region,
+        task_name='deepgenome-agents-enrichment-task',
+        resource_dict=resource_dict,
+        app_id_dict=app_id_dict,
+        compute_resource='small',
+        timeout=timeout,
+        retriable_codes=retriable_codes,
+        max_retries=max_retries,
+        max_poll=max_poll,
+    )
+    return {'enrichment_task': enrichment_task}
 
 
 async def gene_expression_analysis(
