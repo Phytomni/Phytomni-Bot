4f96015658b58a7d6441186428295632260b9643
sssxie <xieshang0608@gmail.com>
2025-08-14 04:13:46 +0800

    ✨ Feat: Add gene function discussion, experiment, and introduction prompts

--- src/mcp_server_phytomni/config/defaults.py
+++ src/mcp_server_phytomni/config/defaults.py
@@ -75,10 +75,15 @@ class ServerConfig(BaseSettings):
 
     REPO_ID: str = Field('a34b2477-a4b1-4a30-8726-77bbf66ca048')
     REPO_ID_DICT: Dict[str, int] = Field({
-        'a34b2477-a4b1-4a30-8726-77bbf66ca048': int(_MAX_TOKENS/2048),
-        'c6aa6922-15ec-44bb-bfaa-3bc85ed4d1a2': int(_MAX_TOKENS/2048),
-        '708b0cf8-fa4d-4ad0-885f-ca3bf4565cda': int(_MAX_TOKENS/2048),
-        'ec3be998-43a8-483e-a2d8-029c9161431b': int(_MAX_TOKENS/2048),
+        'a34b2477-a4b1-4a30-8726-77bbf66ca048': int(_MAX_TOKENS/1024),
+        'ec3be998-43a8-483e-a2d8-029c9161431b': int(_MAX_TOKENS/1024),
+        'd38a792f-58a3-4aff-b521-f04dc6bd06b3': int(_MAX_TOKENS/1024),
+        'c6aa6922-15ec-44bb-bfaa-3bc85ed4d1a2': int(_MAX_TOKENS/1024),
+        '708b0cf8-fa4d-4ad0-885f-ca3bf4565cda': int(_MAX_TOKENS/1024),
+        '7f747eb7-223c-42fa-9cae-431a0bb1a999': int(_MAX_TOKENS/1024),
+        '44ad28b5-5c3b-4a02-8e8c-7fb4903424cb': int(_MAX_TOKENS/1024),
+        '8d7ff2ab-91dd-4d8a-a07d-93729e8c05aa': int(_MAX_TOKENS/1024),
+        '7ee75b57-bf09-4124-9e3a-ddb2070ccb2c': int(_MAX_TOKENS/1024),
     })
 
     WORKSPACE_ID: str = Field('6e939452a68f487f873c457f1953cf55')
@@ -161,8 +166,8 @@ class KnowledgeConfig(ChatConfig):
             reranking is applied to retrieved documents.
     """
     PAGE_NUM: int = Field(1)
-    PAGE_SIZE: int = Field(int(_MAX_TOKENS/2048))
-    TOP_N: int = Field(int(_MAX_TOKENS/2048))
+    PAGE_SIZE: int = Field(int(_MAX_TOKENS/1024))
+    TOP_N: int = Field(int(_MAX_TOKENS/1024))
     FILTER_STRING: Optional[str] = Field(None)
     SCOPE: Literal['both', 'doc', 'keyword'] = Field('both')
     EXTRA_REPO_IDS: Optional[List[str]] = Field(None)
@@ -189,7 +194,7 @@ class DataConfig(KnowledgeConfig):
     NEED_INSIGHT: bool = Field(False)
     SIMPLIFY_RESPONSE: bool = Field(True)
     DIALOG_ID: str = Field('')
-    DATA_REPO_ID: str = Field('daadf7eb-aee7-4a71-a9cc-2884cf3811d2')
+    DATA_REPO_ID: str = Field('a1ea209d-1a00-4d10-9cc5-d1fd8cd490cc')
     DATA_PAGE_SIZE: int = Field(3)
 
 
