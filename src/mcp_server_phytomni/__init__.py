43e43bebbdaf7dd868dfebafa8f2f403138162d4
sssxie <xieshang0608@gmail.com>
2025-08-23 18:19:11 +0800

    🔧 Refactor: Add response_to_string function for RAG response formatting

--- src/mcp_server_phytomni/__init__.py
+++ src/mcp_server_phytomni/__init__.py
@@ -38,7 +38,7 @@ from .data_agents import nl2sql, rewrite_nl2sql
 from .deep_genome_agents import gene_function
 from .in_silico_research_agents import in_silico_research
 from .knowledge_agents import multi_retrieve, multi_retrieve_generate
-from .knowledge_agents import retrieve_generate
+from .knowledge_agents import response_to_string, retrieve_generate
 from .review_agents import deep_research
 from .task_manager import create_task, TaskManager, update_task
 from .utils import download_list_convert, get_prompt, get_token, split_list
@@ -47,6 +47,6 @@ __all__ = ['create_output_dir', 'create_task', 'deep_research',
            'download_list_convert', 'gene_function', 'get_data_list',
            'get_prompt', 'get_token', 'in_silico_research', 'multi_retrieve',
            'multi_retrieve_generate', 'nl2sql', 'phyto_chat',
-           'phyto_chat_with_follow', 'retrieve_generate',
-           'retrieve_plan_submit', 'rewrite_nl2sql', 'split_list',
-           'TaskManager', 'update_task', 'wait_for_completion']
+           'phyto_chat_with_follow', 'response_to_string',
+           'retrieve_generate', 'retrieve_plan_submit', 'rewrite_nl2sql',
+           'split_list', 'TaskManager', 'update_task', 'wait_for_completion']
