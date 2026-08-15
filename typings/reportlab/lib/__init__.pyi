97db2ce0b755cf99104b081abc7d8bcebe03569f
sssxie <xieshang0608@gmail.com>
2026-08-15 08:29:30 +0800

    🧪 Tests: Add ReportLab demo stubs
    - Declare the ReportLab page-size and unit symbols used by the optional deterministic demo generator.
    
    - Track the narrow stub package despite the generic lib directory ignore rule.

---
+++ typings/reportlab/lib/__init__.pyi
@@ -0,0 +1,4 @@
+# Copyright (c) Biotechnology Research Institute,
+# Chinese Academy of Agricultural Sciences. 2024-2026. All rights reserved.
+
+"""Minimal ReportLab library declarations used by demo generation."""
---
+++ typings/reportlab/lib/pagesizes.pyi
@@ -0,0 +1,6 @@
+# Copyright (c) Biotechnology Research Institute,
+# Chinese Academy of Agricultural Sciences. 2024-2026. All rights reserved.
+
+"""Page-size declarations used by the deterministic demo PDF writer."""
+
+LETTER: tuple[float, float]
